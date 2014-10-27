f31a5b469d28f3640aa31246cb8c3f93da6bfae2
Okamensky <rockstone9999@gmail.com>
2014-10-27 18:25:09 +0100

    corrected

--- modeling/kamensky/plot.py
+++ modeling/kamensky/plot.py
@@ -2,12 +2,12 @@ from pylab import *
 import numpy as np
 
 m1 = 5.0 			# central mass
-alfa = 1.0			# accretion parameter
-mass_flow = 0.9			# mass flow from disk
-Rc = 2.0*10.0**-4		# diameter of central object /10**10cm
+alfa = 0.5			# accretion parameter
+mass_flow = 5.0		# mass flow from disk
+Rc = 1.0*10.0**-4		# diameter of central object /10**10cm
 
-Ri = 2.5*10.0**-4		# inner radius
-Rf = 10.0**-2			# outer radius
+Ri = 1.1*10.0**-4		# inner radius
+Rf = 10.0**-1			# outer radius
 Rstep = 0.00001			# step for computing
 
 R_x = []
@@ -19,17 +19,19 @@ Tc = []				# temperature
 TAU = []			# opacity
 NI = []				# viscosity
 Vr = []				# radial speed
+Fx = []
 
 for R in np.arange(Ri,Rf,Rstep):
 	R_x.append(R)
 	f = (1.0-(Rc/R)**(0.5))**(0.25)
-	SIGMA.append(5.2*alfa**(-4/5)*mass_flow**(7/10)*m1**(1/4)*R**(-3/4)*f**(14/5))
-	H.append(1.7*10**8*alfa**(-1/10)*mass_flow**(3/20)*m1**(-3/8)*R**(9/8)*f**(3/5))
-	RHO.append(3.1*10**(-8)*alfa**(-7/10)*mass_flow**(11/20)*m1**(5/8)*R**(-15/8)*f**(11/5))
-	Tc.append(1.4*10**4*alfa**(-1/5)*mass_flow**(3/10)*m1**(1/4)*R**(-3/4)*f**(6/5))
-	TAU.append(190.0*alfa**(-4/5)*mass_flow**(1/5)*f**(4/5))
-	NI.append(1.8*10**14*alfa**(4/5)*mass_flow**(3/10)*m1**(-1/4)*R**(3/4)*f**(6/5))
-	Vr.append(2.7*10**4*alfa**(4/5)*mass_flow**(3/10)*m1**(-1/4)*R**(-1/4)*f**(-14/5))
+	Fx.append(f)
+	SIGMA.append(5.2*alfa**(-4.0/5)*mass_flow**(7.0/10)*m1**(1.0/4)*R**(-3.0/4)*f**(14.0/5))
+	H.append(1.7*10.0**8.0*alfa**(-1.0/10)*mass_flow**(3.0/20)*m1**(-3.0/8)*R**(9.0/8)*f**(3.0/5))
+	RHO.append(3.1*10.0**(-8.0)*alfa**(-7.0/10)*mass_flow**(11.0/20)*m1**(5.0/8)*R**(-15.0/8)*f**(11.0/5))
+	Tc.append(1.4*10.0**4.0*alfa**(-1.0/5)*mass_flow**(3.0/10)*m1**(1.0/4)*R**(-3.0/4)*f**(6.0/5))
+	TAU.append(190.0*alfa**(-4.0/5)*mass_flow**(1.0/5)*f**(4.0/5))
+	NI.append(1.8*10.0**14.0*alfa**(4.0/5)*mass_flow**(3.0/10)*m1**(-1.0/4)*R**(3.0/4)*f**(6.0/5))
+	Vr.append(2.7*10.0**4.0*alfa**(4.0/5)*mass_flow**(3.0/10)*m1**(-1.0/4)*R**(-1.0/4)*f**(-14.0/5))
 	
 matplotlib.rcParams.update({'font.size': 12, 'font.family': 'serif'})
 
@@ -39,6 +41,13 @@ xlabel('R$_{10}$ (R/($10^{10}$ cm))')
 ylabel('$\Sigma$ (g.cm$^{-2}$)')
 plot(R_x,SIGMA)
 savefig("SIGMA.png")
+
+figure(figsize=(8,5))
+title('f')
+xlabel('R$_{10}$ (R/($10^{10}$ cm))')
+ylabel('f')
+plot(R_x,Fx)
+savefig("f.png")
 
 
 figure(figsize=(8,5))
